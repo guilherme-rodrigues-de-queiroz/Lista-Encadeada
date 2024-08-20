1c07bdb8a4fe91b97ca45e97523be58fcf8e04f4
Guilherme Rodrigues <147875655+guilherme-rodrigues-de-queiroz@users.noreply.github.com>
2024-08-20 12:19:55 -0300

    feat: lista-encadeada

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace listaEncadeada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbxValores = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtValorAntigo = new System.Windows.Forms.TextBox();
+            this.txtValorNovo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(104, 78);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(228, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor:";
+            // 
+            // lbxValores
+            // 
+            this.lbxValores.FormattingEnabled = true;
+            this.lbxValores.Location = new System.Drawing.Point(104, 104);
+            this.lbxValores.Name = "lbxValores";
+            this.lbxValores.Size = new System.Drawing.Size(228, 238);
+            this.lbxValores.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Listar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(338, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Excluir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(338, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Alterar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtValorAntigo
+            // 
+            this.txtValorAntigo.Location = new System.Drawing.Point(393, 192);
+            this.txtValorAntigo.Name = "txtValorAntigo";
+            this.txtValorAntigo.Size = new System.Drawing.Size(71, 20);
+            this.txtValorAntigo.TabIndex = 7;
+            // 
+            // txtValorNovo
+            // 
+            this.txtValorNovo.Location = new System.Drawing.Point(393, 229);
+            this.txtValorNovo.Name = "txtValorNovo";
+            this.txtValorNovo.Size = new System.Drawing.Size(71, 20);
+            this.txtValorNovo.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Trocar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(343, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Por isso";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(338, 255);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Existe";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtValorNovo);
+            this.Controls.Add(this.txtValorAntigo);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbxValores);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbxValores;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtValorAntigo;
+        private System.Windows.Forms.TextBox txtValorNovo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listaEncadeada
+{
+    public partial class Form1 : Form
+    {
+        List<string> lista = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lista.Add(txtValor.Text);
+
+            txtValor.Clear();
+            txtValor.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbxValores.Items.Clear();
+
+            for(int i = 0; i < lista.Count; i++)
+            {
+                lbxValores.Items.Add(lista[i]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lista.Remove(txtValor.Text);
+            txtValor.Clear();
+            txtValor.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int posicao = lista.IndexOf(txtValorAntigo.Text);
+            
+            if(posicao < 0)
+            {
+                MessageBox.Show("O valor não foi encontrado!");
+                return;
+            }
+
+            txtValor.Clear();
+            txtValor.Focus();
+            lista[posicao] = txtValorNovo.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (lista.Contains(txtValor.Text))
+            {
+                MessageBox.Show("Valor existe");
+            }
+            else
+            {
+                MessageBox.Show("Valor não existe");
+            }
+        }
+    }
+}
